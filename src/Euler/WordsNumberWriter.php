617eb2595a94ff73ae3f3c2a74a146c9b28ba771
Giorgio Sironi <info@giorgiosironi.com>
2016-02-21 09:33:24 +0100

    Covering all single ciphers

--- src/Euler/WordsNumberWriter.php
+++ src/Euler/WordsNumberWriter.php
@@ -6,6 +6,13 @@ class WordsNumberWriter
     private $ciphers = [
         1 => 'one',
         2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
     ];
 
     public function __invoke($number)
--- test/Euler/WordsNumberWriterTest.php
+++ test/Euler/WordsNumberWriterTest.php
@@ -12,5 +12,12 @@ class WordsNumberWriterTest extends \PHPUnit_Framework_TestCase
     {
         $this->assertEquals('one', $this->writer->__invoke(1));
         $this->assertEquals('two', $this->writer->__invoke(2));
+        $this->assertEquals('three', $this->writer->__invoke(3));
+        $this->assertEquals('four', $this->writer->__invoke(4));
+        $this->assertEquals('five', $this->writer->__invoke(5));
+        $this->assertEquals('six', $this->writer->__invoke(6));
+        $this->assertEquals('seven', $this->writer->__invoke(7));
+        $this->assertEquals('eight', $this->writer->__invoke(8));
+        $this->assertEquals('nine', $this->writer->__invoke(9));
     }
 }
